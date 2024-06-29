73393ee2f4cee63a2db516e7e37f6380b26bbd10
SleepyDopeKnight <chamomiv@student.21-school.ru>
2024-06-30 02:29:15 +0300

    completed facade and builder patterns

---
+++ pattern/01_facade.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+/*
+Применение:
+  Фасад — это структурный паттерн проектирования, который предоставляет простой интерфейс к сложной системе классов,
+  библиотеке или фреймворку и позволяет изолировать код клиента от сложной подсистемы.
+
+Плюсы:
+  1. Снижает зависимость кода клиента от подсистемы.
+  2. Изолирует клиента от компонентов подсистемы.
+  3. Упрощает взаимодействие с подсистемой.
+
+Минусы:
+  1. Фасад может стать объектом, который делает слишком много/связан со всеми классами программы и будет слишком связанный
+     объект, что усложнит его поддержание, тк может что-то сломаться.
+*/
+
+func main() {
+	computer := NewComputer()
+	fmt.Println(computer.startComputer())
+}
+
+func NewComputer() *Computer {
+	return &Computer{cpu: &CPU{}, ram: &RAM{}, vram: &VideoRAM{}}
+}
+
+type Computer struct {
+	cpu  *CPU
+	ram  *RAM
+	vram *VideoRAM
+}
+
+func (c *Computer) startComputer() string {
+	result := []string{c.cpu.startCPU(), c.ram.startRAM(), c.vram.startVideoRam(), "Computer is started"}
+	return strings.Join(result, "\n")
+}
+
+type CPU struct {
+}
+
+func (c *CPU) startCPU() string {
+	return "CPU is started"
+}
+
+type RAM struct {
+}
+
+func (r *RAM) startRAM() string {
+	return "RAM is started"
+}
+
+type VideoRAM struct {
+}
+
+func (vr *VideoRAM) startVideoRam() string {
+	return "Video RAM is started"
+}
---
+++ pattern/02_builder.go
@@ -0,0 +1,88 @@
+package main
+
+import "fmt"
+
+/*
+Применение:
+Строитель — это порождающий паттерн проектирования, который позволяет создавать сложные объекты пошагово.
+Строитель даёт возможность использовать один и тот же код строительства для получения разных представлений объектов.
+Его можно использовать как с классом директор, так и без. Директор только задает последовательность постройки для строителя,
+методы строителя можно вызывать напрямую из клиентского кода, директор удобен при нескольких шаблонах пастройки.
+Используется когда объект имеет сложное создание и конфигурацию.
+
+Плюсы:
+ 1. Позволяет изолировать логику постройки объекта.
+ 2. Позволяет переиспользование кода для постройки разных объектов.
+ 3. Создает объект пошагово
+
+Минусы:
+ 1. Увеличивает и усложняет код из-за введения новых классов и объектов.
+ 2. Если слишком много конфигураций для постройки, то сложно станет управлять и расширять его
+*/
+func main() {
+	builder := &ConcreteBuilder{house: &House{}}
+	director := &Director{}
+	director.SetBuilder(builder)
+	director.Build("Wood floor", "Brick Walls", "Tile roof")
+	house := builder.GetHouse()
+	fmt.Println(house.floor, "\n", house.walls, "\n", house.roof)
+
+	builder.Reset()
+	builder.BuildFloor("Brick floor")
+	builder.BuildWalls("Wood floor")
+	builder.BuildRoof("Tile roof")
+	house = builder.GetHouse()
+	fmt.Println(house.floor, "\n", house.walls, "\n", house.roof)
+}
+
+type Builder interface {
+	BuildFloor(floor string)
+	BuildWalls(walls string)
+	BuildRoof(roof string)
+	GetHouse() *House
+	Reset()
+}
+
+type Director struct {
+	builder Builder
+}
+
+func (d *Director) SetBuilder(builder Builder) {
+	d.builder = builder
+}
+
+func (d *Director) Build(floor, walls, roof string) {
+	d.builder.BuildFloor(floor)
+	d.builder.BuildWalls(walls)
+	d.builder.BuildRoof(roof)
+}
+
+type ConcreteBuilder struct {
+	house *House
+}
+
+type House struct {
+	floor string
+	walls string
+	roof  string
+}
+
+func (b *ConcreteBuilder) BuildFloor(floor string) {
+	b.house.floor = floor
+}
+
+func (b *ConcreteBuilder) BuildWalls(walls string) {
+	b.house.walls = walls
+}
+
+func (b *ConcreteBuilder) BuildRoof(roof string) {
+	b.house.roof = roof
+}
+
+func (b *ConcreteBuilder) GetHouse() *House {
+	return b.house
+}
+
+func (b *ConcreteBuilder) Reset() {
+	b.house = &House{}
+}
