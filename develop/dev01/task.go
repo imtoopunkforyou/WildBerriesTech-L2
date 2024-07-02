34b741cb0a7021eeade7407c54e39560e18fc40f
SleepyDopeKnight <chamomiv@student.21-school.ru>
2024-07-03 02:10:59 +0300

    dev 01 and corrected names for test files

---
+++ develop/dev01/task.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	t, err := GetTime()
+	if err != nil {
+		log.Println("Can't take current time: ", err)
+		os.Exit(1)
+	}
+	fmt.Println("Current time:", t)
+}
+
+func GetTime() (time.Time, error) {
+	t, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		return time.Time{}, err
+	}
+	return t, err
+}
