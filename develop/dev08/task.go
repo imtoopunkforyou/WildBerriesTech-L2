013652843cc5ffed8935a7e765b8346248959785
SleepyDopeKnight <chamomiv@student.21-school.ru>
2024-09-11 02:41:02 +0300

    dev08: added linux pipe's func, need decompose and tests

--- develop/dev08/task.go
+++ develop/dev08/task.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/shirou/gopsutil/process"
+	"io"
 	"os"
 	"os/exec"
 	"strconv"
@@ -13,7 +14,7 @@ import (
 /*
 === Взаимодействие с ОС ===
 
-Необходимо реализовать собственный шелл
+# Необходимо реализовать собственный шелл
 
 встроенные команды: cd/pwd/echo/kill/ps
 поддержать fork/exec команды
@@ -24,34 +25,80 @@ import (
 
 func main() {
 	for {
-		wd := strings.Split(pwd(), "/") // Для вывода конкретной директории разбиваем весь путь на элементы слайса.
+		wd := strings.Split(pwd(), "/")     // Для вывода конкретной директории разбиваем весь путь на элементы слайса.
+		fmt.Print(">/", wd[len(wd)-1], " ") // Выводим конкретную директорию, в которой мы находимся
 
-		fmt.Print(">/", wd[len(wd)-1], " ")  // Выводим конкретную директорию, в которой мы находимся
 		reader := bufio.NewScanner(os.Stdin) // Создаем reader из stdin.
 		if reader.Scan() {                   // Сканируем полученные данные из stdin.
-			New(strings.Fields(reader.Text()), reader.Text()) // Передача reader в виде строки нужно только для echo.
+			handleLinuxPipes(reader.Text())
 		}
 	}
 }
 
-// New Создание нового терминала.
-func New(strSlice []string, str string) {
-	switch strSlice[0] { // Смотрим какая команда пришла.
+func handleLinuxPipes(input string) {
+	if len(input) > 0 {
+		commands := strings.Split(input, "|") // Разбиваем инпут на пайпы.
+
+		var prevReader io.Reader = nil // Начальный поток ввода.
+		for i, command := range commands {
+			if strings.TrimSpace(command) != "" { // Проверяем не пустая ли команда в пайпе.
+				commandSlice := strings.Fields(command)
+
+				if i == len(commands)-1 { // Проверяем последняя/единственная ли команда.
+					execution(commandSlice, prevReader, os.Stdout)
+				} else {
+					pr, pw := io.Pipe() // Создаем пайп для передачи данных между командами.
+					go func(cmd []string, in io.Reader, out io.Writer) {
+						execution(cmd, in, out)
+						pw.Close() // Закрываем writer после выполнения команды.
+					}(commandSlice, prevReader, pw)
+
+					prevReader = pr // Обновляем поток ввода для следующей команды
+				}
+			}
+		}
+	}
+}
+
+// Вызов команд для терминала.
+func execution(str []string, r io.Reader, w io.Writer) {
+	switch str[0] { // Смотрим какая команда пришла.
 	case "pwd":
-		fmt.Println(pwd())
+		fmt.Fprintln(w, pwd())
 	case "echo":
-		fmt.Println(strings.Trim(str, "echo \""))
+		fmt.Fprintln(w, echo(str))
 	case "kill":
-		kill(strSlice)
+		kill(str)
 	case "ps":
-		ps()
+		ps(w)
 	case "cd":
-		cd(strSlice)
+		cd(str)
 	case "\\exit":
 		os.Exit(0)
 	default:
-		fmt.Println(forkExec(strSlice))
+		forkExec(str, r, w)
 	}
+}
+
+func forkExec(str []string, r io.Reader, w io.Writer) {
+	cmd := exec.Command(str[0], str[1:]...) // Вызываем команды из терминала.
+	cmd.Stdin = r                           // Назначаем вводом reader
+	cmd.Stdout = w                          // Назначаем выводом writer
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Fprintln(w, err)
+	}
+}
+
+func echo(str []string) string {
+	var res strings.Builder
+	for _, line := range str[1:] {
+		line = strings.Trim(line, "\"")
+		line = strings.Trim(line, "'")
+		res.WriteString(line + " ")
+	}
+	return res.String()
 }
 
 func pwd() string {
@@ -62,21 +109,13 @@ func pwd() string {
 	return wd
 }
 
-func forkExec(strSlice []string) string {
-	cmd := exec.Command(strSlice[0], strSlice[1:]...) // Вызываем команды из терминала.
-	res, err := cmd.CombinedOutput()                  // Получаем вывод.
-	if err != nil {
-		return err.Error()
+func kill(str []string) {
+	if len(str) < 2 {
+		fmt.Println("kill: not enough arguments")
+		return
 	}
 
-	if len(res) > 0 {
-		return string(res[:len(res)-1]) // Избавляемся от последней пустой строки слайса.
-	}
-	return ""
-}
-
-func kill(strSlice []string) {
-	for _, str := range strSlice[1:] {
+	for _, str := range str[1:] {
 		id, err := strconv.Atoi(str) // Получаем pid из строки.
 		if err != nil {
 			fmt.Println(err)
@@ -96,22 +135,22 @@ func kill(strSlice []string) {
 	}
 }
 
-func ps() {
+func ps(w io.Writer) {
 	proc, err := process.Processes() // Получаем процессы.
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintln(w, err)
 	}
 
-	fmt.Printf("PID\t TIME\t CMD\n")
+	fmt.Fprintf(w, "PID\t TIME\t CMD\n")
 	for _, p := range proc {
 		time, _ := p.Times()  // Получаем время работы процесса.
 		cmd, _ := p.Cmdline() // Получаем имя исполнителя процесса.
-		fmt.Printf("%v\t %0.2f\t %v\n", p.Pid, time.Total(), cmd)
+		fmt.Fprintf(w, "%v\t %0.2f\t %v\n", p.Pid, time.Total(), cmd)
 	}
 }
 
-func cd(strSlice []string) {
-	if len(strSlice) == 1 || strSlice[1] == "~" {
+func cd(str []string) {
+	if len(str) == 1 || str[1] == "~" {
 		home, err := os.UserHomeDir()
 		if err != nil {
 			fmt.Println(err)
@@ -121,7 +160,7 @@ func cd(strSlice []string) {
 			}
 		}
 	} else {
-		if err := os.Chdir(strSlice[1]); err != nil {
+		if err := os.Chdir(str[1]); err != nil {
 			fmt.Println(err)
 		}
 	}
